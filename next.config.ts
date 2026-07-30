7cc63b3902a50ca57ed3d4c4e1091bce9a24d0dc
PC <posedgeclock>
2026-07-31 03:59:08 +0800

    ban httpII

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // 匹配所有路由
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            // 开启 HSTS：告诉所有客户端/浏览器，未来 1 年内禁止对该域名使用明文 HTTP 访问
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
